e7e211d67600ef94ebe1b19bcf74b213743629cc
Zaibo Wang <zaibowang@gmail.com>
2017-10-28 18:16:26 -0400

    Fix bugs for winning

--- game.py
+++ game.py
@@ -7,21 +7,19 @@ class connect4:
 
     # Create a 6 x 7 board
     def __init__(self):
-        self.gamestate = [[' ' for i in range(self.width)] for i in range(self.height)]
-        print(self.gamestate)
+        self.gamestate = [['_' for i in range(self.width)] for i in range(self.height)]
 
     # Places a piece (X or O) in a position (0 to 6)
     # Calls win function to determine if move resulted in win
     def move(self, pos, piece):
         x = pos
         played = False
-        for y in range(self.height, -1, -1):
-            if self.gamestate[y][x] == ' ':
+        for y in range(self.height-1, -1, -1):
+            if self.gamestate[y][x] == '_':
                 played = True
                 self.gamestate[y][x] = piece
                 break
-        # self.win((x,y), piece)
-        return played
+        return (played, self.win((x,y), piece))
 
     def printgame(self):
         for y in range(self.height):
@@ -36,14 +34,14 @@ class connect4:
                 xstart = coord[0]
                 ystart = coord[1]
                 for i in range(3):
-                    if xstart > 0 and xstart <= self.width - 2 and ystart > 0 and ystart <= self.height - 2:
+                    if xstart > 0 and xstart <= self.width - 1 and ystart > 0 and ystart <= self.height - 1:
                         xstart -= dx
                         ystart -= dy
                 count = 0
                 for i in range(self.connect*2 -1):
                     posx = xstart + i*dx
                     posy = ystart + i*dy
-                    if posx > 0 and posx <= self.width - 2 and posy > 0 and posy <= self.height - 2:
+                    if posx >= 0 and posx <= self.width - 1 and posy >= 0 and posy <= self.height - 1:
                         if self.gamestate[posy][posx] == piece:
                             count += 1
                         else:
@@ -55,5 +53,9 @@ class connect4:
         return False
 
 game = connect4()
-game.printgame
-# game.move(3, 5)
+game.printgame()
+print('---------------------------')
+for i in range(4):
+    print(game.move(i, 'O'))
+game.printgame()
+
